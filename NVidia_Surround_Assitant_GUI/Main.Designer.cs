619467f39f80919bbb22aacb453b907067557096
Conrad Gohl <conrad.gohl@gmail.com>
2018-11-10 12:05:43 +0200

    + Release V1.0.0

--- NVidia_Surround_Assitant_GUI/Main.Designer.cs
+++ NVidia_Surround_Assitant_GUI/Main.Designer.cs
@@ -321,7 +321,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "w";
+            this.Text = "NVidia Surround Assistant";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
             this.Load += new System.EventHandler(this.MainForm_Load);
